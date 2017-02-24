0024d9fc318609cfe71ac80ad07c5887bf761707
355 Berry Street <p.mateo@355berrystreet.com>
2017-02-24 07:53:54 +0100

    Initial Commit

---
+++ RecordPro/RecordProController.swift
@@ -0,0 +1,44 @@
+//
+//  RecordProController.swift
+//  RecordPro
+//
+//  Created by Pablo Mateo Fernández on 02/02/2017.
+//  Copyright © 2017 355 Berry Street S.L. All rights reserved.
+//
+
+import UIKit
+
+class RecordProController: UIViewController {
+
+    @IBOutlet private var stopButton: UIButton!
+    @IBOutlet private var playButton: UIButton!
+    @IBOutlet private var recordButton: UIButton!
+    @IBOutlet private var timeLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    // MARK: - Action methods
+    
+    @IBAction func stop(sender: UIButton) {
+        
+    }
+
+    @IBAction func play(sender: UIButton) {
+        
+    }
+
+    @IBAction func record(sender: UIButton) {
+        
+    }
+
+}
